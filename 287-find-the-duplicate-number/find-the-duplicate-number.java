7b9302ae2dacd3ff1122d07cdeb50c09faea23cd
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2025-08-01 17:51:25 +0530

    Time: 1 ms (99.78%) | Memory: 59.5 MB (9.49%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,14 +1,13 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> seen = new TreeSet<>();
+        int n = nums.length;
+        int[] memo = new int[n]; // we use size n instead of n+1 because numbers go from 1 to n
 
         for (int num : nums) {
-            if (!seen.add(num)) {
+            if (memo[num] == 1) {
                 return num; // Duplicate found
             }
+            memo[num] = 1;
         }
 
         return -1; // Should never happen if input is valid
