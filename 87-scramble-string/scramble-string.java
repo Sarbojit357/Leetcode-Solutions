57b63d38c1d2f27f64f891d1b52f1a5f31a49a91
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2025-11-06 20:22:56 +0530

    Time: 79 ms (6.24%) | Memory: 47.2 MB (5.14%) - LeetSync

--- 87-scramble-string/scramble-string.java
+++ 87-scramble-string/scramble-string.java
@@ -5,46 +5,37 @@ class Solution {
     private Map<String, Boolean> memo = new HashMap<>();
 
     public boolean isScramble(String s1, String s2) {
-        
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-
         if (s1.equals(s2)) {
             return true;
+        }
+        if (s1.length() != s2.length()) {
+            return false;
         }
 
         String key = s1 + "#" + s2;
         if (memo.containsKey(key)) {
             return memo.get(key);
         }
-        int[] letters = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            letters[s1.charAt(i) - 'a']++;
-            letters[s2.charAt(i) - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (letters[i] != 0) {
-                memo.put(key, false);
-                return false;
-            }
-        }
 
         int n = s1.length();
-        for (int i = 1; i < n; i++) {
+        boolean scrambled = false;
 
+        for (int i = 1; i < n; i++) {
+            // No swap case
             if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
                 isScramble(s1.substring(i), s2.substring(i))) {
-                memo.put(key, true);
-                return true;
+                scrambled = true;
+                break;
             }
+            // With swap case
             if (isScramble(s1.substring(0, i), s2.substring(n - i)) &&
                 isScramble(s1.substring(i), s2.substring(0, n - i))) {
-                memo.put(key, true);
-                return true;
+                scrambled = true;
+                break;
             }
         }
-        memo.put(key, false);
-        return false;
+
+        memo.put(key, scrambled);
+        return scrambled;
     }
 }
